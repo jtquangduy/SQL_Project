68bbaf8d0d1d412fc1f3562140d24119b3dce896
Quang Duy <justinquangduy@gmail.com>
2024-03-15 19:44:33 +0700

    TestChallenges

---
+++ TestChallenges.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT district
+FROM address
+
+SELECT rental_date
+FROM rental
+ORDER BY rental_date DESC
+LIMIT 1
+
+SELECT COUNT(film_id)
+FROM film
+
+SELECT COUNT(DISTINCT last_name)
+FROM customer
