a244a1396d66dba07da9f06754b779a4b4ea1b19
Quang Duy <justinquangduy@gmail.com>
2024-03-15 19:04:24 +0700

    DISTINCT

---
+++ DISTINCT.sql
@@ -0,0 +1,17 @@
+SELECT DISTINCT first_name, last_name
+FROM customer
+ORDER BY first_name
+
+SELECT DISTINCT 
+rating,
+rental_duration
+FROM film
+
+SELECT *
+FROM payment
+
+SELECT DISTINCT 
+amount
+FROM payment
+ORDER BY amount DESC
+
