067ae2f1b93186f2ac6b9d18917e4407b70037ae
Quang Duy <justinquangduy@gmail.com>
2024-03-16 02:43:57 +0700

    TestChallengesDay2

---
+++ TestChallengesDay2.sql
@@ -0,0 +1,17 @@
+SELECT 
+COUNT(*) AS no_of_movie
+FROM film
+WHERE description LIKE '%Saga%' 
+AND (title LIKE 'A%' OR title LIKE '%R')
+
+SELECT * FROM customer
+WHERE first_name LIKE '_A%ER%'
+ORDER BY last_name DESC 
+
+SELECT * FROM customer
+WHERE first_name LIKE '%ER%' AND first_name LIKE '_A%'
+ORDER BY last_name DESC 
+
+SELECT COUNT(*) FROM payment
+WHERE (amount = 0 OR amount BETWEEN 3.99 AND 7.99 )
+AND (payment_date BETWEEN '2020-05-01' AND '2020-05-02')
